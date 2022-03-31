6f8f8656e5fef7aed3357da6854ecda08ff6966e
onpul <chmj072@gmail.com>
2022-04-01 00:08:36 +0900

    [Bronze III] Title: N 찍기, Time: 296 ms, Memory: 21092 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main 
+{
+	public static void main(String[] args) throws NumberFormatException, IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= num; i++)
+		{
+			bw.write(i + "\n");
+		}
+        
+		bw.flush();
+		bw.close();
+	}
+}
