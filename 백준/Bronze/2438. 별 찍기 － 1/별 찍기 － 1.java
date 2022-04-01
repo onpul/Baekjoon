8c1734593005121e39db83e37b5d85065680117e
onpul <chmj072@gmail.com>
2022-04-01 20:46:54 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 136 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main 
+{
+	public static void main(String[] args) throws NumberFormatException, IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= num; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{		
+				bw.write("*");
+			}
+			
+			bw.write("\n");
+		}
+        
+		bw.flush();
+		bw.close();
+	}
+}
