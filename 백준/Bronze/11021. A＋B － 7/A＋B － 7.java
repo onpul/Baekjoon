2058570bd6da4eb90b6da60e9e70d28fcebcade5
onpul <chmj072@gmail.com>
2022-04-01 20:20:54 +0900

    [Bronze III] Title: A+B - 7, Time: 184 ms, Memory: 16724 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main 
+{
+	public static void main(String[] args) throws NumberFormatException, IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		int i = 1;
+		
+		StringTokenizer st;
+		
+		while (i <= num) 
+		{
+			st = new StringTokenizer(br.readLine()," ");
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			String n = String.format("Case #%d: ", i);
+			
+			bw.write(n + (a + b) + "\n");
+			
+			i++;
+		}
+        
+		bw.flush();
+		bw.close();
+	}
+}
