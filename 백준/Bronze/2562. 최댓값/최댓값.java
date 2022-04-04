f7e0b03c049d7a5e330169d36ba55b04f6381fed
onpul <chmj072@gmail.com>
2022-04-04 21:30:59 +0900

    [Bronze II] Title: 최댓값, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[9];
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		
+		int max = 0;
+		int num = 0;
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			if (arr[i] > max)
+			{
+				max = arr[i];
+				num = i;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(num+1);
+	}
+}
